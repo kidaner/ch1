86051afb976a6bcd9345844dcd91de29ba9be264
Robel Kidane <kidaner@gmail.com>
2022-01-20 05:16:34 -0500

    -A

---
+++ arrayp88.py
@@ -0,0 +1,5 @@
+import array
+
+v = [0.5, 0.75, 1.0, 1.5, 2.0]
+
+a = array.array('f', v)
---
+++ numpyp91.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+a = np.array([0, 0.5, 1.0, 1.5, 2.0])
+
+b = np.array([a, a * 2])
+
+print(b)
