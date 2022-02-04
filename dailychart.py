521b8ef34bb8d110c272b889dc98e8be04653252
Robel Kidane <kidaner@gmail.com>
2022-02-04 17:53:58 -0500

    -A

--- dailychart.py
+++ dailychart.py
@@ -1,6 +1,7 @@
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 
 ticker = input("Please enter ticker: ")
 
@@ -13,5 +14,17 @@ price = company.history(period='1y')
 price = price["Close"]
 
 price_earnings = price / trailingEps
-plt.plot(price, price_earnings)
+
+fig, ax1 = plt.subplots()
+ax1.plot(price, linewidth=3)
+ax1.set_title(f"{ticker} Valuation", fontsize=14)
+ax1.set_xlabel("Date", fontsize=14)
+fig.autofmt_xdate()
+ax1.set_ylabel("Price", fontsize=14)
+ax1.tick_params(axis="both", labelsize=14)
+
+ax2 = ax1.twinx()
+ax2.plot(price_earnings, linewidth=3)
+ax2.set_ylabel("Price/Earnings", fontsize=14)
+
 plt.show()
