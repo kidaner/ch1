3c5b8a475cd308119f91c47c2b1ac01f17b8e7d1
Robel Kidane <kidaner@gmail.com>
2022-03-05 14:51:39 -0500

    -A

---
+++ clar.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+filename = sys.argv[0]
+
+if not os.path.exists(filename):
+    with open(filename, "w") as f:
+        f.write("New file created.\n")
+else:
+    print(f"Error, the file {filename} already exists!")
+    sys.exit(1)
---
+++ subpr.py
@@ -0,0 +1,6 @@
+import subprocess
+
+result = subprocess.run(["host", "8.8.8.8"], capture_output=True)
+
+print(result.returncode)
+print(result.stdout)
